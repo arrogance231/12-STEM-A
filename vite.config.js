40404574892eb0ed3ea3872f13a3ef137c51a0ac
arrogance231 <alchemyofjhine@gmail.com>
2024-03-30 12:22:07 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import {resolve} from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        nested: resolve(__dirname, 'classmates.html'),
+      },
+    },
+  },
   plugins: [react()],
 })
