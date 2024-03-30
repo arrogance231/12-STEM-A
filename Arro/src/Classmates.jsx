32abe052d550833afd1c1983cefe474c37470d34
arrogance231 <alchemyofjhine@gmail.com>
2024-03-30 11:54:59 +0800

    added first row of classmates

--- Arro/src/Classmates.jsx
+++ Arro/src/Classmates.jsx
@@ -1,22 +1,13 @@
 import React from 'react'
-import {Card,Card1,Card2,Card3,Card4,Card5,Card6,Card7,Card8} from './classmates/card.jsx'
-import './classmates/card.css'
 import Nav from './components/Nav.jsx'
+import CardRow1 from './classmates/CardRow1.jsx'
 
 const Classmates = () => {
   return (
-    <div className='flex relative'>
-      <Nav/>
-      <div className='mt-[200px] mx-10 justify-between columns-5'>
-      <Card/>
-      <Card1/>
-      <Card2/>
-      <Card3/>
-      <Card4/>
-      <Card5/>
-      <Card6/>
-      <Card7/>
-      <Card8/>
+    <div className= 'flex w-full'>
+      <nav className='absolute w-full'><Nav/></nav>
+      <div className='mt-[200px] w-full'>
+        <CardRow1/>
       </div>
     </div>
   )
---
+++ Arro/src/classmates/index.js
@@ -0,0 +1,40 @@
+export const classmatesRow1 = [
+    {name: 'Ma. Carmela Angela C. Valdez', picture: '/public/images/Valdez.jpg', motto: 'Success is no accident. It is hard work, perseverance, learning, studying, sacrifice and most of all, love of what you are doing.'},
+    {name: 'John Anthony S. Nagpala', picture: '/public/images/Nagpala.jpg', motto: 'Listen to many, speak to a few. '},
+    {name: 'Jasmin N. Perez', picture: '/public/images/Perez.jpg', motto: 'The better it gets, the better it gets.'},
+]
+
+export const classmatesRow3 = [
+
+
+
+]
+
+export const classmatesRow4 = [
+    {name: '', picture: '', motto: ''}
+
+
+
+]
+
+export const classmatesRow5 = [
+    {name: '', picture: '', motto: ''}
+
+
+
+]
+
+
+export const classmatesRow6 = [
+    {name: '', picture: '', motto: ''}
+
+
+
+]
+
+export const classmatesRow2 = [
+    {name: '', picture: '', motto: ''}
+
+
+
+]
